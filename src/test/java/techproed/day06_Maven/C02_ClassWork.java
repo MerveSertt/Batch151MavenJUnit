8fffb9f2f29594704b92214c30ab7f6ad1e4e83e
merve <mervebalkan34@gmail.com>
2023-06-03 00:34:27 +0300

    02.06.2023 Work

--- src/test/java/techproed/day06_Maven/C02_ClassWork.java
+++ src/test/java/techproed/day06_Maven/C02_ClassWork.java
@@ -2,11 +2,15 @@ package techproed.day06_Maven;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.security.Key;
 import java.time.Duration;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
 public class C02_ClassWork {
     /*
@@ -36,6 +40,43 @@ public class C02_ClassWork {
         WebElement signButton = driver.findElement(By.xpath("//*[@class='icon-signin']"));
         signButton.click();
 
+        //3.Login alanine “username” yazdirin
+        WebElement loginButton = driver.findElement(By.xpath("//input[@id='user_login']"));
+        loginButton.sendKeys("username");
+
+        //4.Password alanine “password” yazdirin
+        WebElement passwordButton = driver.findElement(with(By.tagName("input")).below(loginButton));
+        passwordButton.sendKeys("password",Keys.ENTER);
+
+        driver.navigate().back();
+
+        //6.Online Banking altında Pay Bills sayfasina gidin
+        WebElement onlineBanking = driver.findElement(By.xpath("//li[@id='onlineBankingMenu']"));
+        onlineBanking.click();
+
+        WebElement payBills = driver.findElement(By.xpath("//span[@id='pay_bills_link']"));
+        payBills.click();
+
+        //7.amount kismina yatirmak istediginiz herhangi bir miktari yazin
+        driver.findElement(By.xpath("//input[@id='sp_amount']")).sendKeys("500");
+
+        //8.tarih kismina “2020-09-10” yazdirin
+        driver.findElement(By.xpath("//input[@id='sp_date']")).sendKeys("2020-09-10");
+
+        //9.Pay buttonuna tiklayin
+        driver.findElement(By.xpath("//input[@value='Pay']")).click();
+
+        //10.“The payment was successfully submitted.” mesajinin ciktigini control edin
+        WebElement successfully = driver.findElement(By.xpath("//div[@id='alert_content']"));
+        String actualText = successfully.getText();
+        String expectedText = "The payment was successfully submitted.";
+
+        if (actualText.equals(expectedText)){
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
+        driver.close();
 
 
 
